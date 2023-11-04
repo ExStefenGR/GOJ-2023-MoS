0a307cc8752affd6a4e0c34a762a0ec7a5d580a4
Stefanos T <sttqueries@gmail.com>
2023-11-04 14:43:35 +0000

    Basic Mechanics

--- Kaktos studios/Assets/Programming/PlayerController.cs
+++ Kaktos studios/Assets/Programming/PlayerController.cs
@@ -1,15 +1,28 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 5.0f;  // Movement speed
-    [SerializeField] float jumpForce = 7.0f;  // Jump force
-    private bool isGrounded = false;
+    //Player Navigation Logic
+    [SerializeField] private float speed = 5.0f;  // Movement speed
+    [SerializeField] private float jumpForce = 7.0f;  // Jump force
+    [SerializeField] private int maxWallJumps = 1;  // Maximum wall jumps before touching the groun
+
     private Rigidbody rb;
+    private float horizontalInput;
+    private int wallJumpCount;
+    private int lastWallID = -1;
+    private bool isGrounded;
+    private bool isOnWall;
+
+    //Player Spawn/Checkpoint
+    private Vector3 lastCheckpointPosition;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        wallJumpCount = maxWallJumps;
+        lastCheckpointPosition = transform.position;
     }
 
     void Update()
@@ -17,27 +30,100 @@ public class PlayerController : MonoBehaviour
         ProcessInput();
     }
 
-    void ProcessInput()
+    void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        PerformMovement();
+    }
+
+    private void ProcessInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        // Check for jump input.
+        if (Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            if (isGrounded || (isOnWall && wallJumpCount > 0))
+            {
+                PerformJump();
+                if (isOnWall)
+                {
+                    wallJumpCount--;
+                    isOnWall = false; // Reset the isOnWall to ensure the player cannot cling without re-collision
+                    // Optional: Add additional force away from the wall here
+                }
+            }
         }
+    }
+    private void PerformMovement()
+    {
 
-        Vector3 movement = new(horizontal, 0f, 0f);
-        rb.MovePosition(transform.position + speed * Time.deltaTime * movement);
+            // Use AddForce to move the player based on input and speed.
+            Vector3 force = new(horizontalInput * speed, 0f, 0f);
+            rb.AddForce(force, ForceMode.VelocityChange);
 
+            // Clamp the velocity to ensure that it doesn't exceed the desired speed.
+            rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -speed, speed), rb.velocity.y, rb.velocity.z);
+
+
+    }
+
+    private void PerformJump()
+    {
+        // Perform the jump action.
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        // Reset grounded status after jumping.
+        isGrounded = false;
     }
 
     void OnCollisionStay(Collision collision)
     {
-        isGrounded = true;
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGrounded = true;
+            wallJumpCount = maxWallJumps;
+            lastWallID = -1; // Reset last wall ID
+        }
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            // Prevent clinging to the same wall
+            if (collision.gameObject.GetInstanceID() != lastWallID)
+            {
+                isOnWall = true;
+                lastWallID = collision.gameObject.GetInstanceID(); // Store the current wall ID
+            }
+        }
     }
 
     void OnCollisionExit(Collision collision)
     {
-        isGrounded = false;
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGrounded = false;
+        }
+
+        // When exiting a collision with a wall, set isOnWall to false.
+        // If this is the last wall the player was attached to, reset the wall ID.
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            if (collision.gameObject.GetInstanceID() == lastWallID)
+            {
+                isOnWall = false;
+                lastWallID = -1; // Reset last wall ID, allowing the next wall collision to be considered new
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        /*if (other.CompareTag("Checkpoint"))
+        {
+            lastCheckpointPosition = other.transform.position; // Set the last checkpoint position
+        }
+        */
+        if (other.gameObject.layer == LayerMask.NameToLayer("DeadZone"))
+        {
+            // Move the player to the last checkpoint position
+            rb.position = lastCheckpointPosition;
+            rb.velocity = Vector3.zero;
+        }
     }
 }
