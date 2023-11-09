09ffe3a7776760557f4af5c5cc5ed2559169dd93
Stefanos Triantafyllidis <sttqueries@gmail.com>
2023-11-09 11:18:47 +0000

    Add Moving Platforms

---
+++ Kaktos studios/Assets/Programming/PrefabCode/MovingPlatformController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using UnityEngine;
+
+public class MovingPlatformController : MonoBehaviour
+{
+    [SerializeField] private Transform pointATransform;
+    [SerializeField] private Transform pointBTransform;
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float waitTime = 1.0f;
+    private Vector3 nextPosition;
+    private bool isWaiting;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        // Store the initial positions of the assigned points
+        Vector3 pointAPosition = pointATransform.position;
+        Vector3 pointBPosition = pointBTransform.position;
+
+        // Start by moving towards Point A's position
+        nextPosition = pointAPosition;
+        StartCoroutine(MovePlatform(pointAPosition, pointBPosition));
+    }
+
+    IEnumerator MovePlatform(Vector3 pointAPosition, Vector3 pointBPosition)
+    {
+        while (true) // Loop to move back and forth indefinitely
+        {
+            if (!isWaiting && Vector3.Distance(transform.position, nextPosition) > 0.01f)
+            {
+                // Move towards the next position
+                transform.position = Vector3.MoveTowards(transform.position, nextPosition, speed * Time.deltaTime);
+                yield return null;
+            }
+            else if (!isWaiting)
+            {
+                // Start waiting when the target position is reached
+                isWaiting = true;
+                yield return new WaitForSeconds(waitTime); // Wait for the specified time
+                isWaiting = false;
+
+                // Switch the target position
+                nextPosition = nextPosition == pointAPosition ? pointBPosition : pointAPosition;
+            }
+        }
+    }
+
+        private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerTransform = collision.transform;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Match the player's x position with the platform
+            Vector3 playerPosition = playerTransform.position;
+            playerTransform.position = new Vector3(transform.position.x, playerPosition.y, playerPosition.z);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerTransform = null;
+        }
+    }
+
+}
